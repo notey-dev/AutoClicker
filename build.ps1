abdcac574ab1f09194287a15e098bc7fee172c4f
Notey <info@notey.dev>
2024-12-13 12:24:35 -0600

    task: updated build script

--- build.ps1
+++ build.ps1
@@ -5,7 +5,17 @@ poetry install
 poetry export -f requirements.txt --output requirements.txt
 poetry export -f requirements.txt --with dev --output requirements-dev.txt
 
+# Activate virtual environment
+$venvPath = (poetry env info --path)
+& "$venvPath\Scripts\Activate.ps1"
+
+# Create a blank config.ini if it doesn't exist
+$configPath = "./config.ini"
+if (-Not (Test-Path $configPath)) {
+    New-Item -ItemType File -Path $configPath
+}
+
 # Run PyInstaller
 $distPath = "$env:APPDATA\AutoClicker"
 $icoPath = "./AutoClicker.ico"
-pyinstaller --onefile --windowed --add-data "config.ini;." --distpath $distPath --icon $icoPath autoclicker.py
+pyinstaller --onefile --windowed --add-data "config.ini;." --distpath $distPath --icon $icoPath --collect-all autoclicker autoclicker.py
